c3d8cbb84f458e72de525da59b54542821310112
kuipid01 <kuipid01@gmnail.com>
2024-09-02 12:52:13 +0100

    first page-admin screen

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -7,6 +7,7 @@ import {
 } from "./components";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Chat } from "./components/Chat";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -18,11 +19,12 @@ function App() {
         <div className="app">
           <Navbar />
           <div className="main">
-            <Sidebar />
+            {/* <Sidebar /> */}
             <div className="content">
               <Routes>
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/appointments" element={<Appointments />} />
+                <Route path="/chats" element={<Chat />} />
                 <Route path="/doctors" element={<Doctors />} />
                 <Route path="/" element={<Dashboard />} />
               </Routes>
